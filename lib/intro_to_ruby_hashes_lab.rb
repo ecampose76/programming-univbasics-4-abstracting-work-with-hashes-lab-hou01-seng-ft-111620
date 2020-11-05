324c2b7de5c35bbd88964b4f52ef040028e40693
Elias Campos <ecampose76@gmail.com>
2020-11-05 02:01:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,12 +1,13 @@
 def my_hash_creator(key, value)
   hash = {
-    :key1 = key
+    :key1 => key
   }
   hash[:key1] = value
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  #
+  return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
